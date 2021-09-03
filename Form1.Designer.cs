7e36abf186869785dd629995dd206ab4e559538c
Elman <77393687+Elman295@users.noreply.github.com>
2021-09-03 13:48:15 +0430

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace AYAZgame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cloud = new System.Windows.Forms.PictureBox();
+            this.tree = new System.Windows.Forms.PictureBox();
+            this.bird = new System.Windows.Forms.PictureBox();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.score = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.cloud)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tree)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cloud
+            // 
+            this.cloud.Image = global::AYAZgame.Properties.Resources.cloud_computing;
+            this.cloud.Location = new System.Drawing.Point(359, 116);
+            this.cloud.Name = "cloud";
+            this.cloud.Size = new System.Drawing.Size(114, 73);
+            this.cloud.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cloud.TabIndex = 3;
+            this.cloud.TabStop = false;
+            // 
+            // tree
+            // 
+            this.tree.Image = global::AYAZgame.Properties.Resources.tree;
+            this.tree.Location = new System.Drawing.Point(589, 343);
+            this.tree.Name = "tree";
+            this.tree.Size = new System.Drawing.Size(81, 188);
+            this.tree.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tree.TabIndex = 2;
+            this.tree.TabStop = false;
+            // 
+            // bird
+            // 
+            this.bird.Image = global::AYAZgame.Properties.Resources.peace;
+            this.bird.Location = new System.Drawing.Point(104, 235);
+            this.bird.Name = "bird";
+            this.bird.Size = new System.Drawing.Size(44, 37);
+            this.bird.TabIndex = 1;
+            this.bird.TabStop = false;
+            // 
+            // ground
+            // 
+            this.ground.Image = global::AYAZgame.Properties.Resources.ground_grass_and_dirt1;
+            this.ground.Location = new System.Drawing.Point(-9, 528);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(793, 87);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 0;
+            this.ground.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 20;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label1.Location = new System.Drawing.Point(29, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 22);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "score:";
+            // 
+            // score
+            // 
+            this.score.AutoSize = true;
+            this.score.Location = new System.Drawing.Point(89, 43);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(16, 17);
+            this.score.TabIndex = 5;
+            this.score.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(767, 616);
+            this.Controls.Add(this.score);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cloud);
+            this.Controls.Add(this.tree);
+            this.Controls.Add(this.bird);
+            this.Controls.Add(this.ground);
+            this.Name = "Form1";
+            this.Text = "Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keydown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyup);
+            ((System.ComponentModel.ISupportInitialize)(this.cloud)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tree)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.PictureBox bird;
+        private System.Windows.Forms.PictureBox tree;
+        private System.Windows.Forms.PictureBox cloud;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label score;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AYAZgame
+{
+    public partial class Form1 : Form
+    {
+
+        int speed = 20;
+        int gravity = 5;
+        int s = 0;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+
+            
+            bird.Top += gravity;
+            tree.Left -= 8;
+            cloud.Left -= 8;
+            score.Text = "" + s;
+            if (tree.Left < -50)
+            {
+                tree.Left = 300;
+                s++;
+             
+            }
+            if (cloud.Left < -30)
+            {
+                cloud.Left = 200;
+                s++;
+             
+            }
+            if(bird.Bounds.IntersectsWith(tree.Bounds)||
+                bird.Bounds.IntersectsWith(cloud.Bounds) ||
+                bird.Bounds.IntersectsWith(ground.Bounds))
+            {
+                end();
+            }
+        }
+
+        private void keydown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = -5;
+            }
+
+        }
+
+        private void keyup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 5;
+            }
+        }
+        public void end()
+        {
+            timer.Stop();
+            MessageBox.Show("end game");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
